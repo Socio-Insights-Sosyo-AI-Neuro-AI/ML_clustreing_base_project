1e97b6312eb03ec61e0a01980065ca48735073cd
metinemre <54211029+metinemredonmez@users.noreply.github.com>
2024-05-26 14:25:28 +0300

    updated main

--- main.py
+++ main.py
@@ -23,12 +23,51 @@ class Window(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.width, self.height,self.top,self.left)
         
-        
+        self.widgets()
+        self.tabWidgets()
         self.show()
+    
+    def tabWidgets(self)  
+        self.tabs = QTabWidget()
+        self.setCentralWidgets(self.tabs)
+        self.tab1 = QWidget()
+        self.tabs.addTab(self.tab1,"Main")
         
         
- 
- 
+    def widgets(self): 
+        
+        #plot 
+        self.p = PlotCanvas(self,width=5,height=5)       
+        # q label 
+        self.k_number_text = QLabel( "choose k :")
+        #spin box
+        self.k_number_text = QSpinBox(self)
+        self.k_number_text.setMinimum(1)
+        self.k_number_text.setMinimum(9)
+        self.k_number_text.setSingleStep(1)
+        self.k_number_text.valueChanged.connect(self.k_numberFunction)
+        
+        self.radio_button = QRadioButton("save text",self)
+        self.plot_save = QRadioButton("save plot",self)
+        self.text_plot_save = QRadioButton("save text and plot",self)
+        self.text_plot_save.setChecked(True)
+        
+        #button 
+        self.cluster = QPushButton( "cluster", self)
+        self.cluster.clicked.connect(self.clusterFunction)
+        
+        #list 
+        self.result_list = QListWidget(self)
+        
+        
+   def clusterFunction(self):
+       pass    
+                
+    def k_numberFunction(self):
+        pass    
+
+
+        
 
 #####plot class methods
 
